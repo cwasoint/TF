a886922ca7965c35858187370f02cd007e36690e
cwasoint <101298342+cwasoint@users.noreply.github.com>
2023-07-07 18:43:22 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,6 @@
-// Script para funcionalidades adicionales
-
-// Ejemplo: Cambiar el color del botón de descarga al hacer clic
 const downloadButton = document.querySelector('button');
 
 downloadButton.addEventListener('click', function() {
   downloadButton.style.backgroundColor = '#acc864';
 });
 
-// Agrega más funcionalidades según tus necesidades
